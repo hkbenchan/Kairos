8f0fc39169433c652115746683e73bc6c83785e8
Ben Chan <ustbenchan@gmail.com>
2012-06-18 23:17:26 +0800

    Log added

--- bonfire/application/logs/log-2012-06-17.php
+++ bonfire/application/logs/log-2012-06-17.php
@@ -44,3 +44,17 @@ ERROR - 2012-06-17 16:17:02 --> Severity: Notice  --> Trying to get property of 
 ERROR - 2012-06-17 16:18:24 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_surname /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 46
 ERROR - 2012-06-17 16:18:24 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_nationality_id /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 54
 ERROR - 2012-06-17 16:18:24 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_University /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 56
+ERROR - 2012-06-17 20:57:11 --> Query error: Data truncated for column 'kairosmemberinfo_newsletterUpdate' at row 1
+ERROR - 2012-06-17 20:58:51 --> Severity: Notice  --> Undefined index: kairosmemberinfo_IndustryID /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/models/kairosmemberinfo_model.php 52
+ERROR - 2012-06-17 20:58:51 --> Query error: Column 'IndustryID' cannot be null
+ERROR - 2012-06-17 20:58:51 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/bonfire/codeigniter/core/Exceptions.php:185) /Applications/MAMP/htdocs/bonfire/codeigniter/core/Common.php 442
+ERROR - 2012-06-17 20:59:28 --> Query error: Cannot add or update a child row: a foreign key constraint fails (`KairosDatabase`.`bf_venture`, CONSTRAINT `bf_venture_ibfk_2` FOREIGN KEY (`IndustryID`) REFERENCES `bf_industry` (`iid`) ON UPDATE NO ACTION)
+ERROR - 2012-06-17 21:02:07 --> Query error: Cannot add or update a child row: a foreign key constraint fails (`KairosDatabase`.`bf_venture`, CONSTRAINT `bf_venture_ibfk_2` FOREIGN KEY (`IndustryID`) REFERENCES `bf_industry` (`iid`) ON UPDATE NO ACTION)
+ERROR - 2012-06-17 21:03:11 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_surname /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 46
+ERROR - 2012-06-17 21:03:42 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_surname /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 46
+ERROR - 2012-06-17 21:05:47 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_surname /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/settings/index.php 46
+ERROR - 2012-06-17 21:05:47 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_nationality_id /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/settings/index.php 54
+ERROR - 2012-06-17 21:05:47 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_University /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/settings/index.php 56
+ERROR - 2012-06-17 21:07:06 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_surname /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 46
+ERROR - 2012-06-17 21:14:41 --> Severity: Notice  --> Undefined property: stdClass::$kairosmemberinfo_surname /Applications/MAMP/htdocs/bonfire/modules/kairosmemberinfo/views/content/index.php 46
+ERROR - 2012-06-17 21:18:44 --> Query error: Unknown column 'bf_user_info.id' in 'where clause'
