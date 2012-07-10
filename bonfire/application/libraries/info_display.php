d9c22546e6e73b638eeafb39a12fec3f20e16748
ustbenchan <ustbenchan@gmail.com>
2012-07-10 10:01:19 +0800

    Add config item of url_helper

--- bonfire/application/libraries/info_display.php
+++ bonfire/application/libraries/info_display.php
@@ -1,22 +1,20 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Info_display {
-	
-	public function test()
-	{
-		phpinfo();
-	}
-	
+
 	/* 
 		$name store the sequence of the data that want to display
 		$data the array that contains the data
+		
+		url_index: { row => { header_id => url } }
 	*/
 	
-	public function set_sequence($name,$data)
+	public function set_sequence($name,$data, $url_index = array())
 	{
 		$result_array = array(
 			'header' => $name,
 			'data' => $data,
+			'url' => $url_index,
 		);
 		
 		/*foreach ($name as $display => $data_ref)
