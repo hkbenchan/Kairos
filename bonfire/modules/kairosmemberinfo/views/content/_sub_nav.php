0f5a88a2a5d48f8d7b14e5db2d5e098cd0d1004c
Ben Chan <ustbenchan@gmail.com>
2012-07-13 23:48:16 +0800

    Quick fix on view

--- bonfire/modules/kairosmemberinfo/views/content/_sub_nav.php
+++ bonfire/modules/kairosmemberinfo/views/content/_sub_nav.php
@@ -4,7 +4,12 @@
 	</li>
 	<?php if ($this->auth->has_permission('KairosMemberInfo.Content.Create')) : ?>
 	<li <?php echo $this->uri->segment(4) == 'create' ? 'class="active"' : '' ?> >
-		<a href="<?php echo site_url(SITE_AREA .'/content/kairosmemberinfo/create') ?>" id="create_new"><?php echo lang('kairosmemberinfo_new'); ?></a>
+		<a href="<?php echo site_url(SITE_AREA .'/content/kairosmemberinfo/create') ?>" id="create_new"><?php echo lang('kairosmemberinfo_new_part1'); ?></a>
+	</li>
+	<?php endif; ?>
+	<?php if ($this->auth->has_permission('KairosMemberInfo.Content.Create')) : ?>
+	<li <?php echo $this->uri->segment(4) == 'create_cv' ? 'class="active"' : '' ?> >
+		<a href="<?php echo site_url(SITE_AREA .'/content/kairosmemberinfo/create_cv') ?>" id="create_new"><?php echo lang('kairosmemberinfo_new_cv'); ?></a>
 	</li>
 	<?php endif; ?>
 </ul>
